dcdea04735fc21efb8d2e60f7f58e83c4b378a34
“bluespicyspace” <yannick.stamm@outlook.com>
2024-06-02 09:26:51 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
+// index.js
 import moment from 'moment';
 import simpleGit from 'simple-git';
 import random from 'random';
 import jsonfile from 'jsonfile';
 
-const { writeFile } = jsonfile;
 const FILE_PATH = './data.json';
-
 const git = simpleGit();
 
+// Function to make a commit
 const makeCommit = async n => {
     if (n === 0) {
         await git.push('origin', 'main', { '--set-upstream': true });
         return;
     }
-    const x = random.int(0, 54);
-    const y = random.int(0, 6);
+    const x = random.int(0, 54);  // Weeks in a year
+    const y = random.int(0, 6);   // Days in a week
     const DATE = moment().subtract(1, 'y').add(x, 'w').add(y, 'd').format();
     const data = { date: DATE };
 
-    console.log(data);
+    console.log(`Committing on: ${DATE}`);  // Log commit date for debugging
 
-    writeFile(FILE_PATH, data, async () => {
-        await git.add('.');
+    jsonfile.writeFile(FILE_PATH, data, async () => {
+        await git.add(FILE_PATH);
         await git.commit(DATE, { '--date': DATE });
         makeCommit(--n);
     });
 };
 
+// Initialize and run the script
 const initializeAndRun = async () => {
-    // Initialize the repository if it is not already initialized
     const isRepo = await git.checkIsRepo();
     if (!isRepo) {
         await git.init();
         await git.addRemote('origin', 'https://github.com/BlueSpicySpace/BlueSpicySpace.git');
     }
-    // Check if the current branch has an upstream branch set
+
     const branches = await git.branch(['-vv']);
     if (!branches.all.includes('main')) {
         await git.checkoutLocalBranch('main');
     }
 
-    makeCommit(500);
+    makeCommit(500);  // Number of commits
 };
 
 initializeAndRun();
-
-
